0c8fb9fb47b35b4a52337dda1f93e5d6b70ebb16
someone <someone>
2019-12-13 02:22:00 -0600

    Declaring imaginary classes as I go... lol

--- src/test/java/graphitePencil/WriteTest.java
+++ src/test/java/graphitePencil/WriteTest.java
@@ -11,8 +11,21 @@ import org.junit.Test;
 public class WriteTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void pencilShouldWriteOnBlankPaper() {
+		// Create a pencil object
+		Pencil pencil = new Pencil();
+		
+		// Define what should be written
+		String message = "I can write on paper";
+		
+		// Make pencil write on paper (stored internally)
+		pencil.writeOnPaper(message);
+		
+		// Return current paper pencil is writing on
+		Paper paper = pencil.getPaper();
+		
+		// 
+		assertEquals
 	}
 
 }
