94a2018b2900032d32e14ccd09e2feee1cf037eb
someone <someone>
2019-12-13 02:08:17 -0600

    Let's start simple by deleting auto stuff, add basics

---
+++ src/main/java/graphitePencil/Pencil.java
@@ -0,0 +1,13 @@
+package graphitePencil;
+
+/**
+ * @author Adrian Hernandez
+ */
+public class Pencil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm alive.");
+	}
+
+}
---
+++ src/test/java/graphitePencil/WriteTest.java
@@ -0,0 +1,18 @@
+package graphitePencil;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Adrian Hernandez
+ *
+ */
+public class WriteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
