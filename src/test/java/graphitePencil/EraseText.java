333c34cb0a0d5eb6463284fc57c1aab3851c9508
someone <someone>
2019-12-13 23:05:30 -0600

    So i wrote the test criteria, now i'll go back and make it pass

--- src/test/java/graphitePencil/EraseText.java
+++ src/test/java/graphitePencil/EraseText.java
@@ -8,7 +8,15 @@ public class EraseText {
 
 	@Test
 	public void eraserShouldEraseTextFromPaper() {
+		String text1 = "She told me that she loved me. I said I didn't love her back.";
+		Paper story = new Paper(text1);
 		
+		Eraser.eraseFromPaper(story, "n't");
+		
+		String expected = "She told me that she loved me. I said I did    love her back.";
+		String paperText = story.getText();
+		
+		assertEquals(expected,paperText);
 	}
 
 }
