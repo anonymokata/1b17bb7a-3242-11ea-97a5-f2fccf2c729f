ea3af39e1bc233d824d714c07ecd00470d7fee7b
someone <someone>
2019-12-14 02:32:57 -0600

    Deciding that special white space should be ignored/skipped

--- src/test/java/graphitePencil/EditTest.java
+++ src/test/java/graphitePencil/EditTest.java
@@ -33,4 +33,19 @@ public class EditTest {
 		String paperText = story.getText();
 		assertEquals(expected,paperText);
 	}
+	
+	@Test
+	public void editorShouldNotCollideWithSpecialWhiteSpaceCharacters() {
+		String text1 = "The cow jumped over the moon.\nmooooo!";
+		Paper story = new Paper(text1);
+		
+		int startIndex = 29;
+		String replacement = "oiink";
+		Editor.editOnPaper(story,replacement,startIndex);
+		
+		String expected = "The cow jumped over the moon.\n@@@@oo";
+		String paperText = story.getText();
+		assertEquals(expected,paperText);
+	}
+	
 }
