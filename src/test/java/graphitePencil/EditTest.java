1ddfc4a217b287e83631061037457b44d5daeb67
someone <someone>
2019-12-14 02:08:05 -0600

    Getting sleepy :( made my first edit test

--- src/test/java/graphitePencil/EditTest.java
+++ src/test/java/graphitePencil/EditTest.java
@@ -8,7 +8,16 @@ public class EditTest {
 
 	@Test
 	public void editorShouldEditTextOnPaper() {
+		String text1 = "She       me. She loves me not.";
+		Paper story = new Paper(text1);
 		
+		int startIndex = 4;
+		String newWords = "loves";
+		Editor.editOnPaper(story,"loves",4);
+		
+		String expected = "She loves me. She loves me not.";
+		String paperText = story.getText();
+		assertEquals(expected,paperText);
 	}
 
 }
