71fab813727a68917a609304232d1d37d8ce3bee
someone <someone>
2019-12-14 02:14:52 -0600

    Made changes to try and pass test

--- src/main/java/graphitePencil/Editor.java
+++ src/main/java/graphitePencil/Editor.java
@@ -1,5 +1,20 @@
 package graphitePencil;
 
 public class Editor {
+	public static void editOnPaper(Paper paper, String replacementText, int startIndex) {
+		StringBuilder paperText = new StringBuilder(paper.getText());
+		
+		// Replace literal space characters with letters from replacementText 
+		// @ for any chracter collisions
+		int replacementLength = replacementText.length();
+		for(int i = 0; i < replacementLength; i++) {
+			char currentChar = paperText.charAt(startIndex + i);
+			char replaceChar = replacementText.charAt(startIndex + i);
+			if(currentChar == ' ') {
+				paperText.setCharAt(startIndex + i, replaceChar);
+			}
+		}
+		
+	}
 	
 }
