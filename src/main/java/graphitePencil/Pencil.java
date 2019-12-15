bb63c61ba9af248ac79301282d1bf700e0e15fbc
someone <someone>
2019-12-15 00:46:15 -0600

    Realize i should have had test for writing on paper

--- src/main/java/graphitePencil/Pencil.java
+++ src/main/java/graphitePencil/Pencil.java
@@ -15,12 +15,34 @@ public class Pencil {
 		this.graphitePoints = graphitePoints;
 	}
 
-	public void writeToPaper(Paper paper, String newText) {
-		int lowerCount = 0;
+	public void writeToPaper(Paper paper, String writeText) {
+		StringBuilder degradation = new StringBuilder(writeText);
+		
+		for(int i = 0; i < writeText.length(); i++) {
+			char writeChar = writeText.charAt(i);
+			if(writePoints > 0) {
+				if(Character.isLowerCase(writeChar)) {
+					writePoints--;
+					graphitePoints--;
+				}
+				degradation.append(writeChar);
+			}
+		}
+		
+		String degraded = degradation.toString();
+		Writer.writeToPaper(paper, degraded);
 	}
 
 	public int getWritePoints() {
 		return this.writePoints;
+	}
+	
+	public int getGraphitePoints() {
+		return this.graphitePoints;
+	}
+
+	public int getErasePoints() {
+		return this.erasePoints;
 	}
 
 }
--- src/test/java/graphitePencil/PencilTest.java
+++ src/test/java/graphitePencil/PencilTest.java
@@ -11,14 +11,33 @@ public class PencilTest {
 		Pencil pencil = new Pencil(10,10,100);
 		Paper paper = new Paper();
 		
+		// Test with 1 character
 		pencil.writeToPaper(paper, "a");
 		int availablePoints = pencil.getWritePoints();
-		
 		assertEquals(9,availablePoints);
+		
+		// Test with 3 characters
+		pencil.writeToPaper(paper, "aaa");
+		availablePoints = pencil.getWritePoints();
+		assertEquals(6,availablePoints);		
 	}
 	
+	public void whenWritingPencilShouldNotLosePointsForAnyWhiteSpace() {
+		Pencil pencil = new Pencil(10,10,100);
+		Paper paper = new Paper();
+		
+		// Test with 1 character
+		pencil.writeToPaper(paper, "    ");
+		int availablePoints = pencil.getWritePoints();
+		assertEquals(10,availablePoints);
+		
+		// Test with 3 characters
+		pencil.writeToPaper(paper, "\t\n\r \f  ");
+		availablePoints = pencil.getWritePoints();
+		assertEquals(10,availablePoints);
+	}
 	@Test
-	public void pencilShouldNotWriteWhenOutOfWritePoints() {
+	public void pencilShoulWriteBlankSpacesWhenOutOfWritePoints() {
 		Pencil pencil = new Pencil(10,10,100);
 		Paper paper = new Paper();
 		
