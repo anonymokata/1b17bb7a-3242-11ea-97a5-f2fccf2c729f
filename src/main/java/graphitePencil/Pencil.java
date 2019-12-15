565ae7d6d14b1ab8e16a96b481fb56f69cd2e164
someone <someone>
2019-12-15 00:04:19 -0600

    Realized I should have started by testing letter subtractions

--- src/main/java/graphitePencil/Pencil.java
+++ src/main/java/graphitePencil/Pencil.java
@@ -4,18 +4,19 @@ package graphitePencil;
  * @author Adrian Hernandez
  */
 public class Pencil {
-	private int lifePoints;
+	private int graphitePoints;
 	private int writePoints;
 	private int erasePoints;
 	
 	// Set paper to given string on instantiation
-	Pencil(int writePoints, int erasePoints, int lifPoints){
-		this.lifePoints = 40;
-		this.writePoints = 5;
-		this.erasePoints = 15;
+	Pencil(int writePoints, int erasePoints, int graphitePoints){
+		this.writePoints = writePoints;
+		this.erasePoints = erasePoints;
+		this.graphitePoints = graphitePoints;
 	}
 
 	public void writeToPaper(String newText) {
+		
 	}
 
 
