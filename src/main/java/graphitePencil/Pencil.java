d100428f720a77f5472a69471401bf65e6c3cd46
someone <someone>
2019-12-14 23:47:05 -0600

    Starting Pencil Degradation

--- src/main/java/graphitePencil/Pencil.java
+++ src/main/java/graphitePencil/Pencil.java
@@ -4,35 +4,20 @@ package graphitePencil;
  * @author Adrian Hernandez
  */
 public class Pencil {
-	private Paper paper;
-	
-	// Initialize blank paper if no string given
-	Pencil(){
-		this.setPaper(new Paper());
-	}
+	private int lifePoints;
+	private int writePoints;
+	private int erasePoints;
 	
 	// Set paper to given string on instantiation
-	Pencil(Paper paper){
-		this.setPaper(paper);
+	Pencil(int writePoints, int erasePoints, int lifPoints){
+		this.lifePoints = 40;
+		this.writePoints = 5;
+		this.erasePoints = 15;
 	}
 
 	public void writeToPaper(String newText) {
-		Writer.writeToPaper(getPaper(), newText);
 	}
 
-	/**
-	 * @return the paper
-	 */
-	public Paper getPaper() {
-		return this.paper;
-	}
 
-	/**
-	 * @param paper the paper to use
-	 */
-	public void setPaper(Paper paper) {
-		this.paper = paper;
-	}
-	
 
 }
