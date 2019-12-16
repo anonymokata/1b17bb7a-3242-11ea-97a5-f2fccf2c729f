fa37944318034aaff1c5946149dd68d6c4fba177
someone <someone>
2019-12-16 01:21:41 -0600

    Decided to make an eraser that can handle special whitespace

--- src/test/java/graphitePencil/EditTest.java
+++ src/test/java/graphitePencil/EditTest.java
@@ -7,11 +7,11 @@ import org.junit.Test;
 public class EditTest {
 
 	@Test
-	public void editorShouldEditWhiteSpaceOnPaperWithReplacementString() {
+	public void editorShouldEditBlankSpaceOnPaperWithReplacementString() {
 		String text = "She       me. She loves me not.";
 		Paper story = new Paper(text);
 		
-		// Starting one space after 'She'
+		// Starting 1 space after 'She'
 		int startIndex = 4;
 		String replacement = "loves";
 		Editor.editOnPaper(story,replacement,startIndex);
@@ -20,72 +20,60 @@ public class EditTest {
 		String paperText = story.getText();
 		assertEquals(expected,paperText);
 	}
-	
+
 	@Test
-	public void editorShouldEditSpecialWhiteSpaceCharactersWithReplacementString() {
-		String text = "The cow jumped over the moon.\n\n\f\t\nmooooo!";
+	public void editorShouldNotEditSpecialWhiteSpaceOnPaperWithReplacementString() {
+		// Use WSEditor to edit special white space characters
+		String text = "The cow jumped over the moon.\n\n\f\tmoo!";
 		Paper story = new Paper(text);
 		
-		// Starting at new line in string
+		// Starting at new line
+		int startIndex = 29;
+		String replacement = "oink";
+		Editor.editOnPaper(story,replacement,startIndex);
+		
+		String expected = "The cow jumped over the moon.\n\n\f\tmoo!";
+		String paperText = story.getText();
+		assertEquals(expected,paperText);
+	}
+	
+	@Test
+	public void editorShouldNotCollideWithSpecialWhiteSpaceCharacters() {
+		// WSEditor can write over special whitespace characters
+		String text = "The cow jumped over the moon.\nmooooo!";
+		Paper story = new Paper(text);
+		
+		// Starting at new line
+		int startIndex = 29;
+		String replacement = "oink";
+		Editor.editOnPaper(story,replacement,startIndex);
+		
+		String expected = "The cow jumped over the moon.\n@@@ooo!";
+		String paperText = story.getText();
+		assertEquals(expected,paperText);
+	}	
+	
+	@Test
+	public void editorShouldCollideWithAllNonWhiteSpaceCharactersWhenEditing() {
+		String text = "The cow jumped over the moon.\nmooooo!";
+		Paper story = new Paper(text);
+		
+		//Starting at new line
 		int startIndex = 29;
 		String replacement = "oiink";
 		Editor.editOnPaper(story,replacement,startIndex);
 		
-		String expected = "The cow jumped over the moon.oiinkmooooo!";
+		String expected = "The cow jumped over the moon.\n@@@@oo!";
 		String paperText = story.getText();
 		assertEquals(expected,paperText);
 	}
 
 	@Test
-	public void editorShouldCollideWithAllNonWhiteSpaceCharacters() {
-		String text = "The     jumped over the moon.";
-		Paper story = new Paper(text);
-		
-		// Starting 1 space after 'The'
-		int startIndex = 4;
-		String replacement = "whale";
-		Editor.editOnPaper(story,replacement,startIndex);
-		
-		String expected = "The whal@umped over the moon.";
-		String paperText = story.getText();
-		assertEquals(expected,paperText);
-	}
-	
-	@Test
-	public void editorShouldNotCollideWithWhiteSpaceCharacters() {
-		String text = "The cow jumped over the moon.\nmooooo!";
-		Paper story = new Paper(text);
-		
-		// Starting at new line in string
-		int startIndex = 29;
-		String replacement = "oiink";
-		Editor.editOnPaper(story,replacement,startIndex);
-		
-		String expected = "The cow jumped over the moon.o@@@@oo!";
-		String paperText = story.getText();
-		assertEquals(expected,paperText);
-	}
-	
-	@Test
-	public void editorShouldNotCollideBlankSpacesFromReplacementText() {
-		String text = "The     jumped over the moon.";
-		Paper story = new Paper(text);
-		
-		// Starting 1 space after 'The'
-		int startIndex = 4;
-		String replacement = "cow ran over";
-		Editor.editOnPaper(story,replacement,startIndex);
-		
-		String expected = "The cow @@@p@@e@ver the moon.";
-		String paperText = story.getText();
-		assertEquals(expected,paperText);
-	}
-	
-	@Test
 	public void editorShouldWriteToPaperIfReplacementTextGoesPastEndOfPaper() {
 		String text = "The cow goes ";
 		Paper story = new Paper(text);
 		
+		// Starting after 'goes'
 		int startIndex = 12;
 		String replacement = " moo!";
 		Editor.editOnPaper(story,replacement,startIndex);
@@ -109,6 +97,16 @@ public class EditTest {
 	}
 	
 	@Test
+	public void editorShouldNotCollideBlankSpacesFromReplacementText() {
+		
+	}
+	
+	@Test
+	public void editorShouldNotCollideSpecialWhiteSpaceFromReplacementText() {
+		
+	}
+	
+	@Test
 	public void editorShouldReturnFalseIfStartIndexGreaterThanPaperLength() {
 		Paper story = new Paper();
 		
@@ -124,6 +122,7 @@ public class EditTest {
 		String text = "She       me. She loves me not.";
 		Paper story = new Paper(text);
 		
+		// Start 1 space after 'She'
 		int startIndex = 4;
 		String replacement = "loves";
 		boolean edited = Editor.editOnPaper(story,replacement,startIndex);
