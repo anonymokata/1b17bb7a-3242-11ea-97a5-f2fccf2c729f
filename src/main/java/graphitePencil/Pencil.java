99a2147783d1ee2f75dc1788bb4c248a3c1894ec
someone <someone>
2019-12-16 02:59:13 -0600

    Finished the editOnPaper for pencil, now fleshing out unit tests

--- src/main/java/graphitePencil/Pencil.java
+++ src/main/java/graphitePencil/Pencil.java
@@ -18,35 +18,59 @@ public class Pencil {
 	}
 
 	public void writeToPaper(Paper paper, String writeText) {
-		StringBuilder degradation = new StringBuilder();
+		StringBuilder degradationWriteText = new StringBuilder();
 		
+		// Create a "degrading" version of writeText while tracking writePoints
 		for(int i = 0; i < writeText.length(); i++) {
 			char writeChar = writeText.charAt(i);
-			characterPointWriting(degradation,writeChar);
+			characterPointWriting(degradationWriteText,writeChar);
 		}
 		
-		String degraded = degradation.toString();
-		Writer.writeToPaper(paper, degraded);
+		// Use "degraded" writeText on paper
+		String degradedWriteText = degradationWriteText.toString();
+		Writer.writeToPaper(paper, degradedWriteText);
+	}
+	
+	public void editOnPaper(Paper paper, String replacementText, int startIndex) {
+		StringBuilder degradationReplacementText = new StringBuilder();
+		String paperText = paper.getText();
+		int paperLength = paperText.length();
+		
+		// Create a "degrading" version of replacementText while tracking writePoints 
+		// and inserting '@' where necessary collisions
+		for(int i = 0; i < replacementText.length(); i++) {
+			char replaceChar = replacementText.charAt(i);
+			if(i + startIndex >= paperLength ) {
+				// avoid IndexOutOfBounds error and handle overflow text
+				characterPointWriting(degradationReplacementText,replaceChar);
+			} else {
+				char paperChar = paperText.charAt(startIndex + i);
+				if(!Character.isWhitespace(paperChar)) {
+					// Write a space or @ symbol for collision
+					collisionPointWriting(degradationReplacementText);
+				} else {
+					// regular point processing with whitespace preservation
+					characterPointWriting(degradationReplacementText,replaceChar);
+				}
+			}
+		}
+		
+		String degradedReplacementText = degradationReplacementText.toString();
+		Editor.editOnPaper(paper, degradedReplacementText, startIndex);
 	}
 	
 	public void eraseFromPaper() {
 		
 	}
 	
-	public void editOnPaper() {
-		
-	}
-	
 	private void characterPointWriting(StringBuilder degradation, char writeChar) {
 		char appendChar = writeChar;
 		if(writePoints > 0) {
-			if(Character.isLowerCase(writeChar)) {
-				writePoints--;
-			} else if(Character.isUpperCase(writeChar)) {
+			if(Character.isUpperCase(writeChar)) {
 				if(writePoints > 1) {
 					writePoints -= 2;
 				}  else {
-					// penalize for attempting to write without enough points
+					// Any failed attempts cost a point
 					// essentially zero out and place a blank space
 					writePoints--;						
 					appendChar = ' ';
@@ -56,6 +80,7 @@ public class Pencil {
 				writePoints--;
 			}
 		} else {
+			// no points: preserve white space when possible, blanks for all else
 			if(!Character.isWhitespace(appendChar)) {
 				appendChar = ' ';
 			}
@@ -64,30 +89,44 @@ public class Pencil {
 		degradation.append(appendChar);
 	}
 	
+	private void collisionPointWriting(StringBuilder degradation) {
+		// Collisions will count for 1 point, Any failed attempts cost a point
+		if(writePoints > 0) {
+			degradation.append('@');
+			writePoints--;
+		} else {
+			degradation.append(' ');
+		}
+	}
+	
 	public boolean sharpen() {
-		if(this.pencilLength > 0) {
-			this.writePoints = this.defaultWritePoints;
-			this.pencilLength--;
+		if(pencilLength > 0) {
+			// Only reset if we still have length on pencil
+			writePoints = defaultWritePoints;
+			pencilLength--;
+			
+			// notify user of successful sharpening
 			return true;			
 		} else {
+			// notify user of failed sharpening
 			return false;
 		}
 	}
 
 	public int getWritePoints() {
-		return this.writePoints;
+		return writePoints;
 	}
 
 	public int getErasePoints() {
-		return this.erasePoints;
+		return erasePoints;
 	}
 	
 	public int getPencilLength() {
-		return this.pencilLength;
+		return pencilLength;
 	}
 	
 	public int getDefaultWritePoints() {
-		return this.defaultWritePoints;
+		return defaultWritePoints;
 	}
 
 }
