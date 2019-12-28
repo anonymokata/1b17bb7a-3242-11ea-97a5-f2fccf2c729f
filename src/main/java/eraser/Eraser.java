812a93a1e778c09221f09469ce3b8e4fa2f03ebf
someone <someone>
2019-12-27 21:46:24 -0600

    Finished eraser package jdoc commenting. What is life.

--- src/main/java/eraser/Eraser.java
+++ src/main/java/eraser/Eraser.java
@@ -2,9 +2,23 @@ package eraser;
 
 import paper.Paperable;
 
+/** Eraser Implementation:
+ * Erases replaces all characters with a blank space, but will
+ * leave existing white space, such as new lines, in paper alone.
+ * 
+ * All whitespace will not count towards the boolean return value.
+ * @author Adrian Hernandez
+ */
 public class Eraser extends AbstractEraser {
 
-	// white space on paper preserved
+	/** eraseFromPaper (indexed) Replaces all non-white space characters
+	 * with a blank space. White space in paper is preserved as it was and
+	 * will not be modified by the eraser.
+	 * @param paper The instance of paper that should be edited
+	 * @param startIndex The startIndex to which begin erasing from
+	 * @param endIndex The endIndex to stop erasing at, not inclusive.
+	 * @return true if any character was replaced in paper
+	 */
 	@Override
 	boolean eraseFromPaper(Paperable paper, int startIndex, int endIndex) {
 		// Erase by replacing with spaces using indexes [Inclusive,Exclusive)
--- src/main/java/eraser/StrictEraser.java
+++ src/main/java/eraser/StrictEraser.java
@@ -2,10 +2,22 @@ package eraser;
 
 import paper.Paperable;
 
+/** StrictEraser Implementation:
+ * StrictEraser replaces all characters with a blank space, destroying
+ * any existing whitespace the already existed.
+ * 
+ * Only blank space will not count towards the boolean return value.
+ * @author Adrian Hernandez
+ */
 public class StrictEraser extends AbstractEraser {
 	
-	// replace everything on paper with spaces
-	// white space on paper NOT preserved
+	/** eraseFromPaper (indexed) Replaces all character with a blank space. 
+	 * Existing white space, such as new lines, in paper is destroyed.
+	 * @param paper The instance of paper that should be edited
+	 * @param startIndex The startIndex to which begin erasing from
+	 * @param endIndex The endIndex to stop erasing at, not inclusive.
+	 * @return true if any character was replaced in paper
+	 */
 	@Override
 	boolean eraseFromPaper(Paperable paper, int startIndex, int endIndex) {
 		// Erase by replacing with spaces using indexes [Inclusive,Exclusive)
