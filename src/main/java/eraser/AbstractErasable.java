49ce392ae7face1f55701fcda34a2f3394abe118
someone <someone>
2019-12-27 21:35:42 -0600

    more jdoc commenting on eraser package!

--- src/main/java/eraser/AbstractErasable.java
+++ src/main/java/eraser/AbstractErasable.java
@@ -2,6 +2,27 @@ package eraser;
 
 import paper.Paperable;
 
+/** AbstractErasable has one more required method that enables left to right
+ * degraded erasing. The reason I moved this to a separate method was to
+ * ensure that the text that would have been matched by Erasable.eraseFromPaper
+ * is the same. If we had: 
+ * 1.) original desiredText such as "I am winner"
+ * 2.) only enough points to erase the "winner" portion
+ * 3.) in a paper with text "I am a winner and a winner I shall be."
+ * then Erase.eraseFromPaper(paper,"winner") would not erase that first winner
+ * 
+ * Therefore, I had to make a new method that is able to handle this situation
+ * when having degradation. Woe is me.
+ * @author Adrian Hernandez
+ */
 public interface AbstractErasable extends Erasable {
+	/** eraseFromPaper (portioned) should only erase portion characters counting 
+	 * from the right of the last index of desiredText
+	 * @param paper The instance of paper that should be edited
+	 * @param desiredText The last index of desiredText to erase from paper
+	 * @param portion left to right erasing of text, stop at portion characters 
+	 * counting from the right
+	 * @return true if any character was replaced in paper
+	 */
 	boolean eraseFromPaper(Paperable paper, String desiredText, int portion);
 }
--- src/main/java/eraser/AbstractEraser.java
+++ src/main/java/eraser/AbstractEraser.java
@@ -2,15 +2,24 @@ package eraser;
 
 import paper.Paperable;
 
-// This is my specific implementation that I need to use at AbstractPencil
-// I still wanted the erasing to be separate even if it cost me another
-// class/interface because of a necessary edit function.
-
-// In order to close AbstractEraser from modification, I am making it non public.
-// I had to make a new interface to still allow for extensibility
+/** AbstractEraser is an implementation used within this package that centralizes
+ * all repeated structure and code between StrictEraser and Eraser for DRY's sake.
+ * The difference was from the whitespace handling in eraseFromPaper (indexed). 
+ * 
+ * IMPORTANT NOTES:
+ * I closed off this class from any further modification given that
+ * AbstractErasable implementations rely on eraseFromPaper (indexed), so I
+ * removed the public modifier to make it package protected. It's really just
+ * specific to the package. I then added a public interface so that we are 
+ * still open to extensibility. Should I make my concrete Erasers final?
+ * @author Adrian Hernandez
+ */
 abstract class AbstractEraser implements AbstractErasable {
-	/**
-	 *
+	/** eraseFromPaper will remove the last index of desiredText
+	 * found in paper.
+	 * @param paper The instance of paper that should be erased
+	 * @param desiredText The desiredText that should be erased
+	 * @return return true if ANY character has been erased
 	 */
 	@Override
 	public boolean eraseFromPaper(Paperable paper, String desiredText) {
@@ -19,9 +28,19 @@ abstract class AbstractEraser implements AbstractErasable {
 		if (erasureStart == -1) {return false;}
 		int erasureEnd = erasureStart + desiredText.length();
 		
+		// Reuse of a method that can also be modified if needed for
+		// DRY principle's sake.
 		return eraseFromPaper(paper,erasureStart,erasureEnd);
 	}
 	
+	/** eraseFromPaper (portioned) should only erase portion characters counting 
+	 * from the right of the last index of desiredText
+	 * @param paper The instance of paper that should be edited
+	 * @param desiredText The last index of desiredText to erase from paper
+	 * @param portion left to right erasing of text, stop at portion characters 
+	 * counting from the right
+	 * @return true if any character was replaced in paper
+	 */
 	@Override
 	public boolean eraseFromPaper(Paperable paper, String desiredText, int portion) {
 		// Find last index, returning false if desiredText not found
@@ -34,6 +53,18 @@ abstract class AbstractEraser implements AbstractErasable {
 		return eraseFromPaper(paper,erasureStart,erasureEnd);
 	}
 	
-	// This is for customization within this package only. (IE not public or protected)
+	/** eraseFromPaper (indexed) will always erase the specified range of text from 
+	 * the paper, not including the endIndex. [Inclusive,Exclusive)
+	 * 
+	 * NOTE: This is for customization within this package only. (IE not public or protected)
+	 * I have debated whether or not to add this to an interface or make it public, but have 
+	 * not had a specific need to. It works for the existing structure and I still 
+	 * pass my unit tests soooooo ....  *shrug*
+	 * 
+	 * @param paper The instance of paper that should be edited
+	 * @param startIndex The startIndex to which begin erasing from
+	 * @param endIndex The endIndex to stop erasing at, not inclusive.
+	 * @return true if any character was replaced in paper
+	 */
 	abstract boolean eraseFromPaper(Paperable paper, int startIndex, int endIndex);
 }
