288022ccfb429429b0963a48730b4c76cce20574
someone <someone>
2019-12-31 21:50:24 -0600

    still needing more jdoc comments

--- src/main/java/pencil/AbstractPencil.java
+++ src/main/java/pencil/AbstractPencil.java
@@ -5,13 +5,23 @@ import editor.Editable;
 import writer.Writable;
 import paper.Paperable;
 
-// I believe this should not be public because it only provides customization
-// for different implementations, but more importantly, it does not break from
-// or add anything from Pencilable in the public scope.
+/** AbstractPencil is a base implementation that is used by all classes within
+ * the pencil package. It defines many of the variables and methods that are 
+ * used in modeling a graphite pencil which helps centralize most of the logic
+ * for concrete pencilables found in this package. All the concrete pencilable
+ * classes vary mostly in whitespace handling.
+ * 
+ * IMPORTANT NOTES: 
+ * I believe this should not be public because it only provides customization 
+ * for different implementations, but more importantly, it does not break from
+ * or add anything from Pencilable in the public scope.                       
+ * @author Adrian Hernandez
+ */
 abstract class AbstractPencil
 	implements Pencilable {
 	
-	// it could be beneficial to to have access to this in all subclasses
+	// it could be beneficial to have access to these in ALL subclasses
+	// including those outside of the package extending the concrete classes
 	protected int writePoints;
 	protected int erasePoints;
 	protected int pencilLength;
@@ -20,13 +30,18 @@ abstract class AbstractPencil
 	// Dull Point writing
 	DullStyle writingWithNoPoints;
 	
-	// These are for modular customization of OUR pencil logic.
-	// since this is for our logic simplification, keep it in the package
+	// These are for modular customization of OUR pencil logic
+	// to edit these, use CustomizablePencils instead.
 	AbstractErasable eraser;
 	Editable editor;
 	Writable writer;
 	
-	// sets the inital value and default value of write points at same time
+	/** This constructor initializes the degradation values as specified, while
+	 * setting defaultWritePoints and writePoints to the same value
+	 * @param writePoints the default and initialization for write points of pencil
+	 * @param erasePoints the default and initialization for erase points of pencil
+	 * @param pencilLength the amount of times pencil can be sharpened             
+	 */
 	public AbstractPencil(int writePoints, int erasePoints, int pencilLength){
 		this.writePoints = writePoints;
 		this.erasePoints = erasePoints;
@@ -34,6 +49,12 @@ abstract class AbstractPencil
 		this.defaultWritePoints = writePoints;
 	}
 	
+	/** This constructor initializes the degradation values as specified
+	 * @param writePoints initialization for write points of pencil                      
+	 * @param erasePoints the default and initialization for erase points of pencil      
+	 * @param pencilLength the amount of times pencil can be sharpened                   
+	 * @param defaultWritePoints the default value writePoints is set to after sharpening
+	 */
 	public AbstractPencil(int initialWritePoints, int erasePoints, int pencilLength, int defaultWritePoints) {
 		this.writePoints = initialWritePoints;
 		this.erasePoints = erasePoints;
@@ -111,10 +132,16 @@ abstract class AbstractPencil
 	}
 	
 	
-	// These are helper methods process points and strings for the system 
-	// during major events such as collisions, writing, and erasing
-	// Use only for within the package to create different versions of the "same" 
-	// pencil while following the same structure
+	/* characterPointWriting, characterPointErasing, collisionPointWriting
+	* These are helper methods process points and strings for the system 
+	* during major events such as collisions, writing, and erasing.
+	* Used only within the package to create different versions of the "same" 
+	* pencil while keeping the same structure */
+	
+	/** 
+	 * @param degradation the StringBuilder to add new 'degraded' character
+	 * @param writeChar the character to go through degradation logic
+	 */
 	void characterPointWriting(StringBuilder degradation, char writeChar) {
 		char appendChar = writeChar;
 		if(writePoints > 0) {
@@ -138,6 +165,9 @@ abstract class AbstractPencil
 		degradation.append(appendChar);
 	}
 	
+	/**
+	 * @param degradation
+	 */
 	void collisionPointWriting(StringBuilder degradation) {
 		// Collisions will count for 1 point, Any failed attempts cost a point
 		if(writePoints > 0) {
@@ -148,6 +178,10 @@ abstract class AbstractPencil
 		}
 	}
 	
+	/**
+	 * @param text
+	 * @return
+	 */
 	int characterPointErasing(String text) {
 		int spaceAdjusted = 0;
 		
@@ -172,6 +206,9 @@ abstract class AbstractPencil
 		return spaceAdjusted;
 	}
 	
+	/* These are get methods. These values should NOT be edited directly, therefore
+	 * there are no setter methods. */
+	
 	public int getWritePoints() {
 		return writePoints;
 	}
@@ -189,16 +226,20 @@ abstract class AbstractPencil
 	}
 	
 	
-	// Dull Point processing
+	/* Dull Point processing for whitespace */
+	
+	
+	/**
+	 * @return
+	 */
 	public DullStyle getDullStyle() {
 		return writingWithNoPoints;
 	}
 	
-	public Pencilable setDullStyle(DullStyle style) {
-		writingWithNoPoints = style;
-		return this;
-	}
-	
+	/**
+	 * @param writeChar
+	 * @return
+	 */
 	char dullPointWriting(char writeChar) {
 		if(Character.isWhitespace(writeChar) 
 				&& getDullStyle() == DullStyle.WhiteSpaceCompatible) {
